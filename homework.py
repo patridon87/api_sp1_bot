90bbce8c61fa4dc7bfa2932099ae0d71bd0c37c2
Sergey Karagodin <patridon87@yandex.ru>
2021-08-20 18:30:28 +0300

    after review

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ import requests
 import telegram
 from dotenv import load_dotenv
 import logging
+from http import HTTPStatus
 
 
 logging.basicConfig(
@@ -22,26 +23,49 @@ load_dotenv()
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRAKTIKUM_URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
 
+class TGBotException(Exception):
+    pass
+
+
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    homework_status = homework['status']
-    if homework_status != 'approved':
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    try:
+        homework_name = homework['homework_name']
+        homework_status = homework['status']
+
+        if homework_status == 'reviewing':
+            return 'Работа взята на ревью'
+        elif homework_status == 'rejected':
+            verdict = 'К сожалению, в работе нашлись ошибки.'
+        elif homework_status == 'approved':
+            verdict = 'Ревьюеру всё понравилось, работа зачтена!'
+        else:
+            raise TGBotException('Неизвестный статус домашней работы')
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+
+    except KeyError:
+        raise TGBotException('Сообщение не содержит обязательных полей')
+        logger.error('Сообщение не содержит обязательных полей')
 
 
 def get_homeworks(current_timestamp):
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    url = PRAKTIKUM_URL
     headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     payload = {'from_date': current_timestamp}
-    homework_statuses = requests.get(url, headers=headers, params=payload)
-    return homework_statuses.json()
+
+    try:
+        homework_statuses = requests.get(url, headers=headers, params=payload)
+    except requests.RequestException:
+        raise TGBotException('Сервис "Практикум.Домашка" недоступен')
+
+    if homework_statuses.status_code == HTTPStatus.OK:
+        return homework_statuses.json()
+    else:
+        raise TGBotException('Сетевая ошибка')
 
 
 def send_message(message):
@@ -54,7 +78,8 @@ def main():
     while True:
         try:
             homework = get_homeworks(current_timestamp)
-            if homework['homeworks'] != []:
+            if len(homework['homeworks']) != 0:
+                current_timestamp = int(time.time())
                 last_homework = homework['homeworks'][0]
                 message = parse_homework_status(last_homework)
                 send_message(message)
@@ -62,8 +87,12 @@ def main():
 
             time.sleep(5 * 60)  # Опрашивать раз в пять минут
 
+        except TGBotException as e:
+            logger.error(f'Бот упал с ошибкой {e}')
+            bot.send_message(chat_id=CHAT_ID, text=f'Бот упал с ошибкой {e}')
+            time.sleep(5)
+
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
             logger.error(f'Бот упал с ошибкой {e}')
             bot.send_message(chat_id=CHAT_ID, text=f'Бот упал с ошибкой {e}')
             time.sleep(5)
